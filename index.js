51556816e7d21d2622842fce3ad5985c906bbcc3
Ranjay Kumar <ranjaykumar73.rk@gmail.com>
2025-07-30 18:23:38 +0530

    changes in port

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-app.listen(8080, () => {
-  console.log(`Server is running on port ${8080}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
