c1a331b88791dc7cb7caf4bfdd90f2832b669727
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2025-08-06 17:58:54 +0530

    edit

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get("/", (req, res) => {
 
 app.use((err, req, res, next) => {
   console.error("Error occurred: ", err);
-  const statusCode = err.statusCode || 5008;
+  const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   res.status(statusCode).json({ message }); // Respond with the error message
 });
